4ec4bb11e64e5a21be61f91379bfee977f1dfcf2
naychishunnlei <naychishunnlei@gmail.com>
2024-12-27 17:08:47 +0700

    hhh

--- lab5_1.py
+++ lab5_1.py
@@ -6,8 +6,8 @@ from PySide6.QtGui import *
 class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
-        self.setWindow("Simple GitHub Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.setWindowTitle("Simple GitHub Drawing")
+        self.rabbit = QPixmap("images/rabbit.jpg")
 
     def paintEvent(self, e):
         p = QPainter()
@@ -27,6 +27,30 @@ class Simple_drawing_window(QWidget):
         p.drawPolygon(
             [QPoint( 50, 200), QPoint(150, 200), QPoint(100, 400),]
         )
+
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple GitHub Drawing2")
+        self.rabbit = QPixmap("images/rabbit.jpg")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+
+        p.drawPolygon(
+            [QPoint( 50, 200), QPoint(150, 200), QPoint(100, 400),]
+        )
+
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0, 0, 127))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
 
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
